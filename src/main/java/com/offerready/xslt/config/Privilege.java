d4c8302de33a9158059667750423a83c9e988540
Adrian Smith <adrian.m.smith@gmail.com>
2018-02-18 16:16:21 +0100

    Make code more concise by using Lombok @Data

--- src/main/java/com/offerready/xslt/config/Privilege.java
+++ src/main/java/com/offerready/xslt/config/Privilege.java
@@ -1,16 +1,16 @@
 package com.offerready.xslt.config;
 
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+import lombok.Data;
+
 import javax.annotation.Nonnull;
 import java.io.Serializable;
 
-/** Just a string, but having a type makes static type checking better and increases readability. */
-@SuppressWarnings("serial")
+@Data
+@SuppressFBWarnings("RCN_REDUNDANT_NULLCHECK_OF_NONNULL_VALUE")
 public class Privilege implements Comparable<Privilege>, Serializable {
+
     protected @Nonnull String name;
-    public Privilege(@Nonnull String n) { name = n; }
-    public @Nonnull String getName() { return name; }
-    @Override @Nonnull public String toString() { return name; }
-    @Override public boolean equals(Object obj) { return obj instanceof Privilege && ((Privilege)obj).name.equals(name); }
-    @Override public int hashCode() { return name.hashCode(); }
+
     public int compareTo(@Nonnull Privilege o) { return name.compareTo(o.name); }
 }
