7e45aae42eb302a0b0d84ff740e4c9efee2f6a91
Adrian Smith <adrian.m.smith@gmail.com>
2018-12-05 10:16:39 +0100

    2.3.0: Add getters for BufferedDocumentGenerationDestination fields

--- src/main/java/com/offerready/xslt/BufferedDocumentGenerationDestination.java
+++ src/main/java/com/offerready/xslt/BufferedDocumentGenerationDestination.java
@@ -1,5 +1,7 @@
 package com.offerready.xslt;
 
+import lombok.Getter;
+
 import javax.annotation.CheckForNull;
 import javax.annotation.Nonnull;
 import java.io.ByteArrayOutputStream;
@@ -7,9 +9,9 @@ import java.io.OutputStream;
 
 public abstract class BufferedDocumentGenerationDestination implements DocumentGenerationDestination {
 
-    protected String contentType;
-    protected @CheckForNull String filenameOrNull = null;
-    protected ByteArrayOutputStream body = null;
+    protected @Getter String contentType;
+    protected @Getter @CheckForNull String filenameOrNull = null;
+    protected @Getter ByteArrayOutputStream body = null;
 
     @Override public void setContentType(@Nonnull String contentType) { this.contentType = contentType; }
     @Override public void setContentDispositionToDownload(@CheckForNull String filename) { this.filenameOrNull = filename; }
